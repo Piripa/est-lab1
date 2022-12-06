1de5c6a84a15f1b9a90f3ba1643a98fa738aeb10
Vinicius Alessandro <103452537+Piripa@users.noreply.github.com>
2022-12-06 20:56:16 +0000

    Adicionado Insert,Search e quase delete

--- ListaArray.java
+++ ListaArray.java
@@ -1,20 +1,51 @@
 public class ListaArray implements EstruturaDeDados{
 
+    private int[] elements;
+    private int contador;
+
+    public ListaArray(){
+        elements = new int[1000];
+        contador = 0;
+    }
+
+    public int getElemento(int valor){
+        return elements[valor];
+    }
+
     @Override
-    public boolean insert(int chave) {
-        // TODO Auto-generated method stub
-        return false;
+    public boolean insert(int chave){
+        
+        if(contador == 1000){
+            return false;
+        }
+        this.elements[contador]=chave;
+        contador++;
+        return true;
+
     }
 
     @Override
     public boolean delete(int chave) {
-        // TODO Auto-generated method stub
+        for(int i = 0; i < contador; i++){
+            if(this.elements[i] == chave){
+                this.elements[i] = 0;
+                for(int j =0; j<elements.length;j++){
+                    int x = elements[i];
+                    
+                }
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean search(int chave) {
-        // TODO Auto-generated method stub
+        for(int i = 0; i < contador; i++){
+            if(elements[i] == chave){
+                return true;
+            }
+        }
         return false;
     }
 
@@ -44,5 +75,11 @@ public class ListaArray implements EstruturaDeDados{
 
     public static void main(String[] args) {
         ListaArray r = new ListaArray();
+        r.insert(1);
+        System.out.println(r.getElemento(0));
+        r.search(1);
+        
+        
+        
     }
 }
